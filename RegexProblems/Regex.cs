41f499ea98a0ead352b0aa6a6ccacdeda6530042
Diwakar <diwakarn1599@gmail.com>
2021-07-14 00:05:34 +0530

    [Diwakar] Validate last name using regex

--- RegexProblems/Regex.cs
+++ RegexProblems/Regex.cs
@@ -26,5 +26,24 @@ namespace RegexProblems
                 }
             }
         }
+        public static void ValidateLastName()
+        {
+            string[] lastName = { "Diwa", "Raju", "Manu", "Jack", "ma", "arun", "Ja", "1sa", "RRaj", "Diwakaaaar" };
+            string s = @"^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(s);
+
+            foreach (string i in lastName)
+            {
+                Match res = regex.Match(i);
+                if (res.Success)
+                {
+                    Console.WriteLine($"Valid --> {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"InValid --> {i}");
+                }
+            }
+        }
     }
 }
