beb49029b5b864b3c14e466c0c8a69d5e544b134
Diwakar <diwakarn1599@gmail.com>
2021-07-14 00:02:37 +0530

    [Diwakar] Validate First name using regex

---
+++ RegexProblems/Regex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+    class RegexProblem
+    {
+        public static void ValidateFirstName()
+        {
+            string[] firstName = { "Diwa", "Raju", "Manu", "Jack", "ma", "arun","Ja","1sa","RRaj","Diwakaaaar" };
+            string s = @"^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(s);
+
+            foreach( string i in firstName)
+            {
+                Match res = regex.Match(i);
+                if (res.Success)
+                {
+                    Console.WriteLine($"Valid --> {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"InValid --> {i}");
+                }
+            }
+        }
+    }
+}
